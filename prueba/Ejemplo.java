743f79751edd38ed336bcc0151dbb5fa3b05220c
Hpoveda77 <hanas.povedac@unac.edu.co>
2025-03-03 21:37:26 -0500

    archivo java modificado

---
+++ prueba/Ejemplo.java
@@ -0,0 +1,27 @@
+class Ejemplo {
+    int numero;
+    String nombre;
+    
+    public Ejemplo(int numero, String nombre) {
+        this.numero = numero;
+        this.nombre = nombre;
+    }
+    
+    public int getNumero() {
+        return numero;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+}
